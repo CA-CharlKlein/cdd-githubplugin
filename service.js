97eb2b0c26a3a2970dc05af0cb85caa6cb29eb27
CA-CharlKlein <Charl.Klein@CaAfrica.co.za>
2018-04-13 07:06:50 +0200

    Clean Up and Comments

--- service.js
+++ service.js
@@ -1,11 +1,14 @@
-// A Service to get the Import Github Content from a Specific Repository
-// Service will respond to HTTP requests with a  JSON
-module['exports'] = function cddGithubHook(hook) {
-    // hook.req is a Node.js http.IncomingMessage
-    var host = hook.req.host;
-    // hook.res is a Node.js httpServer.ServerResponse
-    // Respond to the request with a simple string
+// Filename: service.js
+// Creator: Charl Klein (CA Southern Africa)
+//
+// A Hooki.io Service to get the Import Github Content from a Specific Repository for CA Continious Delivery Director
+// Service will respond to HTTP requests with a  JSON Formatted Document
 
+// Node JS Module Definition
+module['exports'] = function cddGithubHook(hook) {
+    var host = hook.req.host;
+
+    //Invoke our Request Handler
     requestHandler(hook);
 };
 
@@ -13,16 +16,14 @@ module['exports'] = function cddGithubHook(hook) {
 function requestHandler(hook) {
     var request = require('request');
 
+    // Extract the Parameters from the hook Object.
     var params = hook.params;
     var operation = hook.params.operation;
 
-
-
-    console.log(params);
-
-
-
+    // Switch Based on Operation Passed,
+    // These are defined in Manifest.json
     switch (operation) {
+        //Simple Connectivity Test to Github, to Validate Repo Onwer, Name and User-Agent
         case 'connectivity-test':
             /* 
             SAMPLE REQUEST
@@ -40,6 +41,7 @@ function requestHandler(hook) {
             "ip": "162.243.187.144"
             } */
 
+            // Define Connection Parameters
             var githubConnection = {
                 url: 'https://api.github.com/repos/' + params.endPointProperties.repositoryOwner + '/' + params.endPointProperties.repository + '/issues',
                 headers: {
@@ -47,11 +49,14 @@ function requestHandler(hook) {
                 }
             };
 
+            // Execute Request to Git
             request.get({
                 'url': githubConnection.url,
                 'headers': githubConnection.headers
             }, function (err, res, body) {
                 if (err) {
+
+                    // Error Handling
                     var errorMessage = {
                         success: false,
                         errorMessage: err.messsage
@@ -65,13 +70,13 @@ function requestHandler(hook) {
                     success: true,
                     errorMessage: 'null'
                 }
+
+                //Return Success Message :)
                 hook.res.setHeader('content-type', 'application/json');
                 hook.res.end(JSON.stringify(successMessage));
             })
             break;
         case 'content-items':
-
-
             /* 
             SAMPLE REQUEST:
               {
@@ -93,7 +98,7 @@ function requestHandler(hook) {
                 "ip": "162.243.187.144"
             }*/
 
-
+            // Define Github Connection with Query Sting Parameters
             var githubConnection = {
                 url: 'https://api.github.com/repos/' + params.endPointProperties.repositoryOwner + '/' + params.endPointProperties.repository + '/issues',
                 headers: {
@@ -106,12 +111,15 @@ function requestHandler(hook) {
                 }
             };
 
+            // Execute Request to Git
             request.get({
                 'url': githubConnection.url,
                 'headers': githubConnection.headers,
                 'qs': githubConnection.queryString
             }, function (err, res, body) {
                 if (err) {
+
+                    // Error Handling
                     var errorMessage = {
                         success: false,
                         errorMessage: err.messsage
@@ -121,32 +129,46 @@ function requestHandler(hook) {
                     hook.res.end(JSON.stringify(errorMessage));
                 }
 
-                var elements = JSON.parse(body);
+                console.log(res.headers);
 
-                var contentItems = [];
+                // Rate Limiting Handler
+                if (res.headers.status != "403 Forbidden") {
 
-                elements.forEach(function (item) {
-                    var includeItem = false;
+                    // Parse Response from Git
+                    var elements = JSON.parse(body);
 
+                    // Define Array for Content Items
+                    var contentItems = [];
 
-                    contentItems.push({
-                        content: item.title,
-                        type: "TASK",
-                        displayType: item.labels[0].name,
-                        status: item.state,
-                        id: item.number,
-                        externalId: item.id
+                    // Loop through Git issues and Complie array
+                    elements.forEach(function (item) {
+                        var includeItem = false;
+
+                        // Add item to the Array
+                        contentItems.push({
+                            content: item.title,
+                            type: ((params.contentSourceProperties.issueLabel.indexOf("bug") > -1) ? "DEFECT" : "TASK"),
+                            displayType: item.labels[0].name,
+                            status: item.state,
+                            id: item.number,
+                            externalId: item.id
+                        })
+
                     })
+                    // Define Output Object
+                    var outputContent = {
+                        contents: contentItems
+                    }
 
-                })
+                    //Resure response is application/json
+                    hook.res.setHeader('content-type', 'application/json');
+                    hook.res.end(JSON.stringify(outputContent));
+                } else {
 
-
-                var quickcontent = {
-                    contents: contentItems
+                    // Return Error for rate limiting
+                    hook.res.statusCode = 400;
+                    hook.res.end("Error - Rate Limit Hit");
                 }
-
-                hook.res.setHeader('content-type', 'application/json');
-                hook.res.end(JSON.stringify(quickcontent));
             })
 
             break;
